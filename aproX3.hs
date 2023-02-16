b42fb5aefd35963bb0db611759d661fcc2db3d0b
BomberBeetle <gdreks@gmail.com>
2023-02-16 11:32:06 -0300

    generic riemann method

--- aproX3.hs
+++ aproX3.hs
@@ -1,5 +1,6 @@
 calcRiemann x bound step = if x>=bound then 0 else (x*x) + calcRiemann (x+step) bound step
 
+approxGeneric x f bound step = if x>=bound then 0 else ((f x)*step + approxGeneric (x+step) f bound step)
 calcIntegration = (4*4*4)/3
 
 calcError = calcIntegration - calcRiemann 0 4 1
