b0c173a4d5ac69610c8898bd3f24a542dd36c3f6
BomberBeetle <gdreks@gmail.com>
2023-03-16 11:27:52 -0300

    safe secant

--- secant.hs
+++ secant.hs
@@ -1,3 +1,7 @@
 secantApprox f x xprev tol
     | abs (f x) < tol = x
     | otherwise = secantApprox f (x-(f x)*((x - xprev)/(f x - f xprev))) x tol
+
+secantApproxSafe f x xprev tol depth max_depth
+    | (abs (f x) < tol) || (depth >= max_depth) = x
+    | otherwise = secantApproxSafe f (x-(f x)*((x - xprev)/(f x - f xprev))) x tol (depth+1) max_depth
