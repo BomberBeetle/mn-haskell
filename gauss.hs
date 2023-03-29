18a2c25800b9c689ab39769f8f06b9902fb872b6
BomberBeetle <gdreks@gmail.com>
2023-03-29 12:05:32 -0300

    almost there

--- gauss.hs
+++ gauss.hs
@@ -1,5 +1,5 @@
 gauss :: [[Double]] -> [[Double]]
-gauss lst 
+gauss lst
     | length lst < 2 = lst
     | length (head lst) < 2 = lst
     | otherwise= pivot:(fmap (\x -> 0:x) (gauss $ fmap (tail) nullified))
@@ -23,5 +23,48 @@ nullifyFirstLayer pivot (x:xs) = (zipWith (\a pivotA -> a - (pivotA*multiFactor)
 
 nullifyFirstLayer pivot [] = []
 
+solveForXs lst
+    | detExtended triLst == 0 = Nothing
+    | otherwise = Just (reverse $ innerSolve [] $ reverse lst) --Now I have to actually solve this.
+    where triLst = gauss lst
 
+innerSolve :: (Fractional a, Eq a)=> [a] -> [[a]] -> [a]
+innerSolve coefs (x:xs) = innerSolve (coefs ++ [solvedVar]) xs
+    where solvedVar = ((last x)- sum (zipWith (*) (take (length coefs) (clipZeros x)) coefs))/(reverse x !! 1)
 
+innerSolve coefs [] = []
+
+clipZeros (x:xs) = if x == 0 then clipZeros xs else x:xs
+clipZeros [] = []
+
+detExtended extMatrix = determinant $ fmap (init) extMatrix
+
+--kibe abaixos
+
+sPermutations :: [a] -> [([a], Int)]
+sPermutations = flip zip (cycle [1, -1]) . foldl aux [[]]
+  where
+    aux items x = do
+      (f, item) <- zip (cycle [reverse, id]) items
+      f (insertEv x item)
+    insertEv x [] = [[x]]
+    insertEv x l@(y:ys) = (x : l) : ((y :) <$>) (insertEv x ys)
+
+elemPos :: [[a]] -> Int -> Int -> a
+elemPos ms i j = (ms !! i) !! j
+
+prod
+  :: Num a
+  => ([[a]] -> Int -> Int -> a) -> [[a]] -> [Int] -> a
+prod f ms = product . zipWith (f ms) [0 ..]
+
+sDeterminant
+  :: Num a
+  => ([[a]] -> Int -> Int -> a) -> [[a]] -> [([Int], Int)] -> a
+sDeterminant f ms = sum . fmap (\(is, s) -> fromIntegral s * prod f ms is)
+
+determinant
+  :: Num a
+  => [[a]] -> a
+determinant ms =
+  sDeterminant elemPos ms . sPermutations $ [0 .. pred . length $ ms]
